20fbefdbd5e565d5890c7acac6054860ec4914a1
tradepro-john <jfsdunglao@gmail.com>
2018-09-22 12:10:47 +0800

    initial model

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Movie(models.Model):
+
+	title = models.CharField(max_length=100, unique=True)
+	summary = models.TextField()
+	date_screened = models.DateField()
+	# director = models.ManyToManyField(Director)
+	director = models.CharField(max_length=100)
+
+	likes = models.PositiveInteger(default=0)
+
+	created_at = models.DateTimeField(auto_now_add=True)
+	last_updated = models.DateTimeField(auto_now=True)
+
+
+
