64a3b0d358020648e2eeef98a99aa82295b8065d
tradepro-john <jfsdunglao@gmail.com>
2018-09-22 12:14:08 +0800

    typo on models

--- movie/models.py
+++ movie/models.py
@@ -12,7 +12,7 @@ class Movie(models.Model):
 	# director = models.ManyToManyField(Director)
 	director = models.CharField(max_length=100)
 
-	likes = models.PositiveInteger(default=0)
+	likes = models.PositiveIntegerField(default=0)
 
 	created_at = models.DateTimeField(auto_now_add=True)
 	last_updated = models.DateTimeField(auto_now=True)
